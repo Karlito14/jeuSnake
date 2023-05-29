d022094551b9c5e2d900eb72b71fa25b7f054226
Karlito14 <leiroz26@hotmail.com>
2023-05-29 22:35:20 +0200

    création classe constructeur drawing

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let snakee;
-let applee;
 window.onload = () => {
     const canvas = document.createElement('canvas');
-    // Je récupère le contexte du canvas et je dessine en 2D
     const context = canvas.getContext('2d');
     let delay = 150;
     const canvasWidth = 900;
@@ -13,99 +10,69 @@ window.onload = () => {
     const centreX = canvasWidth / 2;
     const centreY = canvasHeight / 2;
     let score = 0;
+    let snakee;
+    let applee;
     let timeout;
 
-    const gameOver = () => {
-        delay = 150
-        context.save();
-        context.font = 'bold 70px sans-serif';
-        context.fillStyle = 'black';
-        context.textAlign = 'center';
-        context.textBaseline = 'middle';
-        context.fillText('Game Over', centreX, centreY - 180);
-        context.font = 'bold 30px sans-serif';
-        context.fillText('Appuyer sur \'Espace\' pour rejouer', centreX, centreY - 120);
-        context.restore();
-    };
+    class Drawing {
+        static gameOver(context, centreX, centreY) {
+            delay = 150
+            context.save();
+            context.font = 'bold 70px sans-serif';
+            context.fillStyle = 'black';
+            context.textAlign = 'center';
+            context.textBaseline = 'middle';
+            context.fillText('Game Over', centreX, centreY - 180);
+            context.font = 'bold 30px sans-serif';
+            context.fillText('Appuyer sur \'Espace\' pour rejouer', centreX, centreY - 120);
+            context.restore();
+        };
 
-    const launch = () => {
-        snakee = new Snake([[6,4], [5,4], [4,4]], 'right');
-        applee = new Apple([10,10]);
-        clearTimeout(timeout);
-        RefreshCanvas();
-    };
+        static drawScore(context, centreX, centreY, score) {
+            context.save();
+            context.font = 'bold 200px sans-serif';
+            context.fillStyle = 'gray';
+            context.textAlign = 'center';
+            context.textBaseline = 'middle';
+            context.fillText(score.toString(), centreX, centreY);
+            context.restore();
+        };
 
-    const drawScore = () => {
-        context.save();
-        context.font = 'bold 200px sans-serif';
-        context.fillStyle = 'gray';
-        context.textAlign = 'center';
-        context.textBaseline = 'middle';
-        context.fillText(score.toString(), centreX, centreY);
-        context.restore();
-    };
+        static drawBlock (context, position, blockSize) {
+            const [xCoord, yCoord] = position;
+            context.fillRect(xCoord * blockSize, yCoord * blockSize, blockSize, blockSize);
+        };
 
-    const init = () => {
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-        canvas.style.border = '30px solid gray';
-        canvas.style.display = 'block';
-        canvas.style.margin = '50px auto';
-        canvas.style.backgroundColor = '#ddd';
-        document.body.appendChild(canvas);
-        launch();
-    };
-
-    const speed = () => {
-        delay /= 1.1;
-    }
-
-    const RefreshCanvas = () => {
-        snakee.advance();
-        if(snakee.checkCollision()){
-            score = 0;
-            gameOver();
-        } else {
-            if(snakee.isEatingApple(applee)) {
-                score++;
-                snakee.ateApple = true;
-                if (score % 5 === 0){
-                    speed();
-                }
-                do {
-                    applee.setNewPosition();
-                }
-                while(applee.isOnSnake(snakee));
+        static drawSnake (context, blockSize, snake) {
+            context.save();
+            context.fillStyle= '#ff0000';
+            for(let block of snake.body) {
+                this.drawBlock(context, block, blockSize);
             }
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            drawScore();
-            snakee.draw();
-            applee.draw();
-            timeout = setTimeout(RefreshCanvas, delay);
-        }   
-    }
+            context.restore();
+        };
 
-    const drawBlock = (context, position) => {
-        const xCoord = position[0] * blockSize;
-        const yCoord = position[1] * blockSize;
-        context.fillRect(xCoord, yCoord, blockSize, blockSize);
+        static drawApple (context, blockSize, apple) {
+            context.save();
+            context.fillStyle = '#33cc33';
+            context.beginPath();
+            const radius = blockSize / 2 ;
+            const xCoord = apple.position[0] * blockSize + radius;
+            const yCoord = apple.position[1] * blockSize + radius;
+            context.arc(xCoord, yCoord, radius, Math.PI * 2, false);
+            context.fill();
+            context.restore();
+        };
     }
 
     class Snake {
 
-        constructor(body, direction) {
+        constructor(body, direction = 'right') {
             this.body = body;
             this.direction = direction;
             this.ateApple = false;
         }
-        draw () {
-            context.save();
-            context.fillStyle= '#ff0000';
-            for(let i = 0; i < this.body.length; i++) {
-                drawBlock(context, this.body[i]);
-            }
-            context.restore();
-        };
+        
         advance () {
             const nextPosition = this.body[0].slice();
             switch(this.direction){
@@ -154,10 +121,8 @@ window.onload = () => {
         checkCollision () {
             let wallCollision = false;
             let snakeCollision = false;
-            const headSnake = this.body[0];
-            const restSnake = this.body.slice(1);
-            const headSnakeX = headSnake[0];
-            const headSnakeY = headSnake[1];
+            const [headSnake, ...restSnake] = this.body;
+            const [headSnakeX, headSnakeY] = headSnake;
             const minWallX = 0;
             const minWallY = 0;
             const maxWallX = widthInBlock - 1;
@@ -169,8 +134,8 @@ window.onload = () => {
                 wallCollision = true;
             }
 
-            for(let i = 0; i < restSnake.length; i++) {
-                if(headSnakeX === restSnake[i][0] && headSnakeY === restSnake[i][1]){
+            for(let rest of restSnake) {
+                if(headSnakeX === rest[0] && headSnakeY === rest[1]){
                     snakeCollision = true;
                 }
             }
@@ -188,20 +153,10 @@ window.onload = () => {
     }
 
     class Apple {
-        constructor(position){
+        constructor(position = [10, 10]){
             this.position = position;
         }
-        draw () {
-            context.save();
-            context.fillStyle = '#33cc33';
-            context.beginPath();
-            const radius = blockSize / 2 ;
-            const xCoord = this.position[0] * blockSize + radius;
-            const yCoord = this.position[1] * blockSize + radius;
-            context.arc(xCoord, yCoord, radius, Math.PI * 2, false);
-            context.fill();
-            context.restore();
-        };
+        
         setNewPosition () {
             const newX = Math.round(Math.random() * (widthInBlock - 1));
             const newY = Math.round(Math.random() * (heighInBlock - 1));
@@ -210,14 +165,61 @@ window.onload = () => {
         isOnSnake (snake) {
             let isOnSnake = false;
 
-            for(let i = 0; i < snake.body.length; i++) {
-                if(this.position[0] === snake.body[i][0] && this.position[1] === snake.body[i][1]){
+            for(let body of snake.body) {
+                if(this.position[0] === body[0] && this.position[1] === body[1]){
                     isOnSnake = true;
                 }
             };
             return isOnSnake;
         }
     } 
+
+    const launch = () => {
+        snakee = new Snake([[6,4], [5,4], [4,4]]);
+        applee = new Apple();
+        clearTimeout(timeout);
+        RefreshCanvas();
+    };
+
+    const init = () => {
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        canvas.style.border = '30px solid gray';
+        canvas.style.display = 'block';
+        canvas.style.margin = '50px auto';
+        canvas.style.backgroundColor = '#ddd';
+        document.body.appendChild(canvas);
+        launch();
+    };
+
+    const speed = () => {
+        delay /= 1.1;
+    }
+
+    const RefreshCanvas = () => {
+        snakee.advance();
+        if(snakee.checkCollision()){
+            score = 0;
+            Drawing.gameOver(context, centreX, centreY);
+        } else {
+            if(snakee.isEatingApple(applee)) {
+                score++;
+                snakee.ateApple = true;
+                if (score % 5 === 0){
+                    speed();
+                }
+                do {
+                    applee.setNewPosition();
+                }
+                while(applee.isOnSnake(snakee));
+            }
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            Drawing.drawScore(context, centreX, centreY, score);
+            Drawing.drawSnake(context, blockSize, snakee);
+            Drawing.drawApple(context, blockSize, applee);
+            timeout = setTimeout(RefreshCanvas, delay);
+        }   
+    }
 
     init();  
 
